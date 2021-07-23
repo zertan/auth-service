1c97916bd748535bc8f6e47eb54ad519a931bf27
Daniel Hermansson <dhermans@redhat.com>
2021-07-23 15:25:20 +0200

    Fix userinfo endpoint

--- src/authsvc/handlers.clj
+++ src/authsvc/handlers.clj
@@ -14,10 +14,19 @@
   (response/redirect (str (:forward-url request) "?" (:query-string request))))
 
 (defn forward [request forward-url]
-  (client/request (merge request
-                         {:url (str forward-url (:uri request))})))
+  (let [h (conj (:headers request) {"x-forwarded-for" (:remote-addr request)
+                                    ;"x-forwarded-host" (:remote-addr request)
+                                    "host" forward-url})
+        r (-> request
+              (assoc-in [:headers] h)
+              (assoc :server-name forward-url)
+              (assoc :url (str forward-url (:uri request))))]
+    (println "forwarding request: " r)
+    ;(client/request r)
+    ))
 
 (defn token [request]
+  (println "sending request to token endpoint: " request)
   (let [ctx (atom {:url (-> @config/config :sso :url)})
         cr ((middleware/wrap-client-id (fn [x] x)) (merge request {:context ctx}))
         a (client/request (merge request {:url (str (-> @config/config :sso :url) (:uri request))
@@ -26,4 +35,6 @@
         c (client/request (merge (auth/decision-request @ctx) {:url (str (-> @config/config :sso :url) "/protocol/openid-connect/token")}))]
     (go (<! (timeout 500))
         (actions/set-section-access @ctx))
+    (println "got token response: " a)
+    (println "got decision request: " c)
     a))
--- src/authsvc/routes.clj
+++ src/authsvc/routes.clj
@@ -1,5 +1,6 @@
 (ns authsvc.routes
   (:require [authsvc.handlers :refer :all]
+            [clj-http.lite.client :as client]
             [authsvc.config :as config]
             [ring.util.response :as response]))
 
@@ -8,4 +9,13 @@
     ["/protocol/openid-connect/" {"token" token
                                   "auth" (fn [request]
                                            (response/redirect (str forward-url (:uri request) "?" (:query-string request))))
-                                  "userinfo" (fn [request] (forward request forward-url))}]))
+                                  "userinfo" (fn [request]
+                                               (println "req from 3scale: " request)
+                                               (let [token (get (:headers request) "authorization")
+                                                     request2 {:url (str (-> @config/config :sso :url) "/protocol/openid-connect/userinfo")
+                                                               :method :get
+                                                               :headers {"Authorization" token}}]
+                                                 (println "forwarding request: " request2)
+                                                 (client/request request2)
+                                                 )) ;(fn [request] (forward request forward-url))
+                                  }]))
